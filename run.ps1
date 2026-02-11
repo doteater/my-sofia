a9767029abb7e062c5fbfddc35ae921dcc080d5f
doteater <3267474+doteater@users.noreply.github.com>
2026-02-11 14:59:41 -0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -25,13 +25,11 @@ Function Elevate-Script {
 # Invoke Self-Elevation
 Elevate-Script
 
+
 $URL = 'https://github.com/doteater/my-sofia/raw/refs/heads/main/sc-sofia-7.1.0-260210.zip'
 $filename = 'C:\windows\temp\s.zip'
 irm $URL -o $filename
 expand-archive $filename
-$URL = 'https://github.com/doteater/my-sofia/raw/refs/heads/main/sc-sofia-7.1.0-260210.zip'
-$filename = 'C:\windows\temp\s.zip'
-irm $URL -o $filename
-expand-archive $filename
+get-childitem -recurse .\s | unblock-file
 cd s\Sophia.Script.for.Windows.11*\Sophia_Script_for_Windows_11*
 .\sophia.ps1
