228bbe781ac0a65f2e9938d032ec9a34b57bd7f6
doteater <3267474+doteater@users.noreply.github.com>
2026-02-11 14:38:00 -0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,1 +1,6 @@
-
+$URL = 'https://github.com/doteater/my-sofia/raw/refs/heads/main/sc-sofia-7.1.0-260210.zip'
+$filename = 'C:\windows\temp\s.zip'
+irm $URL -o $filename
+expand-archive $filename
+cd s\Sophia.Script.for.Windows.11*\Sophia_Script_for_Windows_11*
+.\sophia.ps1
